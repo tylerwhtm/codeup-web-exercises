cdab6906bf7b6dc9c3588a7ea137e29aa2229d3a
Tyler Whitman <2tylerwhitman4@gmail.com>
2022-11-22 11:51:37 -0600

    test commit

---
+++ js/external.js
@@ -0,0 +1,10 @@
+alert("Welcome to my Website!");
+let prompt1 = prompt("whats your favorite color");
+alert(`Great, ${prompt1} is my favorite color too!`);
+
+let lm = prompt("how many days are you renting The little Mermaid?");
+let bb = prompt("how many days are you renting brother bear?");
+let h = prompt("how many days are you renting Hercules?");
+let price = ((parseInt(lm) + parseInt(bb) + parseInt(h)) * 3)
+alert(`${price} is the total cost`)
+
